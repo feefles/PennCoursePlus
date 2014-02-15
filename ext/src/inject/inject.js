dfd9453d610f1a28d38f7d8112dde5be1ae3c745
Evelyn Fifi Yeung <eyeung@seas.upenn.edu>
2014-02-15 16:52:02 -0500

    finished instructor quality

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -36,6 +36,7 @@ var readyStateCheckInterval = setInterval(function() {
 
 				$(".pitDarkDataTable thead tr").append('<th style="background-color: #666666;font-weight: bold; color:#dcdcdc;">Difficulty</th>');
 				$(".pitDarkDataTable thead tr").append('<th style="background-color: #666666;font-weight: bold; color:#dcdcdc;">Quality</th>');
+				$(".pitDarkDataTable thead tr").append('<th style="background-color: #666666;font-weight: bold; color:#dcdcdc;">Professor</th>');
 
 				var courseList = $('.pitDarkDataTable tbody').children();
 
@@ -43,6 +44,8 @@ var readyStateCheckInterval = setInterval(function() {
 					var that = this;
 					var courseId = $(this).find('td:nth-child(1)').text();
 					var courseType = $(this).find('td:nth-child(3)').text();
+					var inst = $(this).find('td:nth-child(4)').text().toLowerCase();
+
 
 
 					var courseId = courseId.trim();
@@ -54,66 +57,56 @@ var readyStateCheckInterval = setInterval(function() {
 						url: 'http://api.penncoursereview.com/v1/depts/'+courseDept+'/reviews?token=VUFSbua5RgR7Ers7wrivV0MklE48sP',
 						dataType: 'json'
 					}).done(function(data) {
-						// console.log(data)
-						var classes = data.result.values;
-						var qualityavg = 0;
-						var difficultyavg = 0;
-						var numcourses = 0;
-						classes.forEach(function(name) {
-							var alias = name.section.primary_alias.trim().slice(0,-4);
-							alias = alias.replace(/\s/g,'');
-							if (courseId == alias) {
-								qualityavg+=parseInt(name.ratings.rCourseQuality);
-								difficultyavg+=parseInt(name.ratings.rDifficulty);
-								numcourses++;
+						// if (courseType.trim() !== 'Recitation') {
+							var classes = data.result.values;
+							var qualityavg = 0;
+							var difficultyavg = 0;
+							var numcourses = 0;
+
+							profavg = 0;
+							numprofcourses = 0;
+							classes.forEach(function(name) {
+								var instructor = name.instructor.name.toLowerCase();
+								if (instructor.trim() == inst.trim()) {
+									profavg += parseFloat(name.ratings.rInstructorQuality);
+                   					numprofcourses++;
+								}
+
+
+
+								var alias = name.section.primary_alias.trim().slice(0,-4);
+								alias = alias.replace(/\s/g,'');
+								if (courseId == alias) {
+									qualityavg+=parseFloat(name.ratings.rCourseQuality);
+									difficultyavg+=parseFloat(name.ratings.rDifficulty);
+									numcourses++;
+								}
+							});
+							qualityavg = (qualityavg/numcourses).toFixed(2);
+							difficultyavg = (difficultyavg/numcourses).toFixed(2);
+							profavg = (profavg/numprofcourses).toFixed(2);
+							if (isNaN(qualityavg)) {
+								qualityavg = "N/A";
 							}
-						});
-						qualityavg = (qualityavg/numcourses).toFixed(2);
-						difficultyavg = (difficultyavg/numcourses).toFixed(2);
-						if (isNaN(qualityavg)) {
-							qualityavg = "N/A";
+							if (isNaN(difficultyavg)) {
+								difficultyavg = "N/A";
+							}
+							if (isNaN(profavg)) {
+								profavg = "N/A";
+							}
+							if (courseType.trim() !== "Recitation") {
+								$(that).append("<td>"+difficultyavg+"</td>");
+								$(that).append("<td>"+qualityavg+"</td>");
+								$(that).append("<td>"+profavg+ "</td>");
 						}
-						if (isNaN(difficultyavg)) {
-							difficultyavg = "N/A";
-						}
-						if (courseType.trim() !== "Recitation") {
-						$(that).append("<td>"+difficultyavg+"</td>");
-						$(that).append("<td>"+qualityavg+"</td>");
-					}
+						// }
+					
 				});
 				});
 
            $('.pitDarkDataTable').tablesorter({});
 
 		}
-
-
-
-
-
-
-			/*var courseId = $(this).find('td:first').text();
-			$(this).append("<td>"+courseId+"</td>");
-			$(this).append("<td>"+courseId+"</td>");
-
-			var split = courseId.trim().slice(0,-4);
-				// split = getRating(split);
-
-				split = split.replace(/\s/g,''); 
-				$.ajax({
-					type: 'GET',
-					url: 'http://api.penncoursereview.com/v1/coursehistories/'+split+'?token=public',
-					dataType: 'json'
-				}).done(function(data) {
-					var info = data.result.courses.name;
-                    $(that).append("<td>"+info+"</td>");
-	
-                    ////////////// */
-
-
-
-			// ----------------------------------------------------------
-
 		}
 	}
 }, 10);
