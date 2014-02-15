9cd5d1babecd7269dc734ed1c7208879e5cddf29
Jonathan Dubin <saveprobe@gmail.com>
2014-02-14 23:39:06 -0500

    yo

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,17 +1,30 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
 
-		var c = $(".pitDarkDataTable tr:first td").length;
-		$(".pitDarkDataTable tr:first").append("<td><a href=''>Delete</a> Col "+(c+1)+"</td>");
-		$(".pitDarkDataTable tr:gt(0)").append("<td>Col</td>");
+		if ($(".pitDarkDataTable").length > 0) {
+			console.log("hello world");
+
+
+			$(".pitDarkDataTable tr:first").append('<td style="background-color: #666666;font-weight: bold; color:#dcdcdc;">Rating</td>');
+			var courseList = $('.pitDarkDataTable').children().children().next();
+
+			$(courseList).each(function(){
+				var courseId = $(this).find('td:nth-child(1)').text();
+				$(this).append("<td>"+courseId+"</td>");
+			});
+		/*var c = $(".pitDarkDataTable tr:first td").length;
+		$(".pitDarkDataTable tr:first").append("<td><Col "+(c+1)+"</td>");
+		$(".pitDarkDataTable tr:gt(0)").append("<td>Col</td>");*/
+	}
+
+
 		// ----------------------------------------------------------
 
 	}
-	}, 10);
+}, 10);
 });
