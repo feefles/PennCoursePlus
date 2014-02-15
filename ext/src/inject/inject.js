d54140d55efb571a473e43cb19595fe992e5093e
Jonathan Dubin <saveprobe@gmail.com>
2014-02-14 22:03:03 -0500

    yo

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -6,6 +6,10 @@ chrome.extension.sendMessage({}, function(response) {
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
+
+		var c = $(".pitDarkDataTable tr:first td").length;
+		$(".pitDarkDataTable tr:first").append("<td><a href=''>Delete</a> Col "+(c+1)+"</td>");
+		$(".pitDarkDataTable tr:gt(0)").append("<td>Col</td>");
 		// ----------------------------------------------------------
 
 	}
