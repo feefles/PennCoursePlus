1bd7eaffcb9dc38ce5a7aef198c3601e434374e0
Jonathan Dubin <saveprobe@gmail.com>
2014-02-17 21:52:25 -0500

    fixed bug whereby sort was not working

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,4 +1,3 @@
-
 chrome.extension.sendMessage({}, function(response) {
 
     // var getRating = function(course) {
@@ -12,117 +11,119 @@ chrome.extension.sendMessage({}, function(response) {
     // )};
 
 
-var readyStateCheckInterval = setInterval(function() {
-	if (window.location.href.indexOf("pennintouch.apps.upenn.edu") > -1) {
+    /*
+To implement
+	1. Link to PCR site
+*/
+
+    var readyStateCheckInterval = setInterval(function() {
+        if (window.location.href.indexOf("pennintouch.apps.upenn.edu") > -1) {
 
 
 
-		if (document.readyState === "complete") {
-			clearInterval(readyStateCheckInterval);
+            if (document.readyState === "complete") {
+                clearInterval(readyStateCheckInterval);
 
-			// ----------------------------------------------------------
-			// This part of the script triggers when page is done loading
+                // ----------------------------------------------------------
+                // This part of the script triggers when page is done loading
 
-			if ($(".pitDarkDataTable").length > 0) {
+                if ($(".pitDarkDataTable").length > 0) {
+
+                    $('.pitDarkDataTable tr:first').unwrap().wrap("<thead/>");
 
 
-				$('.pitDarkDataTable tr:first').unwrap().wrap("<thead/>");
+                    $('.pitDarkDataTable tr:nth-child(2)').nextUntil(".pitDarkDataTable tr:last").andSelf().wrapAll("<tbody/>");
 
+                    $(".pitDarkDataTable thead tr").append('<th style="background-color: #666666;font-weight: bold; color:#dcdcdc;">Difficulty</th>');
+                    $(".pitDarkDataTable thead tr").append('<th style="background-color: #666666;font-weight: bold; color:#dcdcdc;">Quality</th>');
+                    $(".pitDarkDataTable thead tr").append('<th style="background-color: #666666;font-weight: bold; color:#dcdcdc;">Professor</th>');
 
-				$('.pitDarkDataTable tr:nth-child(2)').nextUntil(".pitDarkDataTable tr:last").andSelf().wrapAll("<tbody/>");
-
-				$(".pitDarkDataTable thead tr").append('<th style="background-color: #666666;font-weight: bold; color:#dcdcdc;">Difficulty</th>');
-				$(".pitDarkDataTable thead tr").append('<th style="background-color: #666666;font-weight: bold; color:#dcdcdc;">Quality</th>');
-				$(".pitDarkDataTable thead tr").append('<th style="background-color: #666666;font-weight: bold; color:#dcdcdc;">Professor</th>');
-
-				/*$(".pitDarkDataTable thead tr").append('<th>Difficulty</th>');
+                    /*$(".pitDarkDataTable thead tr").append('<th>Difficulty</th>');
 				$(".pitDarkDataTable thead tr").append('<th>Quality</th>');*/
 
-				var courseList = $('.pitDarkDataTable tbody').children();
+                    var courseList = $('.pitDarkDataTable tbody').children();
 
-				$(courseList).each(function(){
-					var that = this;
-					var courseId = $(this).find('td:nth-child(1)').text();
-					var courseType = $(this).find('td:nth-child(3)').text();
-					var inst = $(this).find('td:nth-child(4)').text().toLowerCase();
-					inst = inst.replace(/\./g, '');
+                    $(courseList).each(function() {
 
-
-
-					var courseId = courseId.trim();
-					courseId = courseId.slice(0,-4).replace(/\s/g,''); 
-					courseDept = courseId.slice(0,-4).toLowerCase();
-					// split = getRating(split);
-					$.ajax({
-						type: 'GET',
-						url: 'http://api.penncoursereview.com/v1/depts/'+courseDept+'/reviews?token=qL_UuCVxRBUmjWbkCdI554grLjRMPY',
-						dataType: 'json'
-					}).done(function(data) {
-						if (courseType.trim() !== 'Recitation' && courseType.trim() !== 'Laboratory') {
-							var classes = data.result.values;
-							var qualityavg = 0;
-							var difficultyavg = 0;
-							var numcourses = 0;
-
-							profavg = 0;
-							numprofcourses = 0;
-							classes.forEach(function(name) {
-								var instructor = name.instructor.name.toLowerCase();
-								instructor = instructor.replace(/\./g, '');
-
-								if (instructor.trim() == inst.trim()) {
-									profavg += parseFloat(name.ratings.rInstructorQuality);
-                   					numprofcourses++;
-								}
+                        var that = this;
+                        var courseId = $(this).find('td:first').text();
+                        var courseType = $(this).find('td:nth-child(3)').text();
+                        var inst = $(this).find('td:nth-child(4)').text().toLowerCase();
+                        inst = inst.replace(/\./g, '');
 
 
+                        var courseId = courseId.trim();
+                        courseId = courseId.slice(0, -4).replace(/\s/g, '');
+                        courseDept = courseId.slice(0, -4).toLowerCase();
+                        // split = getRating(split);
+                        $.ajax({
+                            type: 'GET',
+                            url: 'http://api.penncoursereview.com/v1/depts/' + courseDept + '/reviews?token=qL_UuCVxRBUmjWbkCdI554grLjRMPY',
+                            dataType: 'json',
+                            async: false
+                        }).done(function(data) {
 
-								var alias = name.section.primary_alias.trim().slice(0,-4);
-								alias = alias.replace(/\s/g,'');
-								if (courseId == alias) {
-									qualityavg+=parseFloat(name.ratings.rCourseQuality);
-									difficultyavg+=parseFloat(name.ratings.rDifficulty);
-									numcourses++;
-								}
-							});
-							qualityavg = (qualityavg/numcourses).toFixed(2);
-							difficultyavg = (difficultyavg/numcourses).toFixed(2);
-							profavg = (profavg/numprofcourses).toFixed(2);
-							if (isNaN(qualityavg)) {
-								qualityavg = "N/A";
-							}
-							if (isNaN(difficultyavg)) {
-								difficultyavg = "N/A";
-							}
-							if (isNaN(profavg)) {
-								profavg = "N/A";
-							}
-							
-								$(that).append("<td>"+difficultyavg+"</td>");
-								$(that).append("<td>"+qualityavg+"</td>");
-								$(that).append("<td>"+profavg+ "</td>");
-						}
-						else {
-						$(that).append("<td>"+" "+"</td>");
-						$(that).append("<td>"+" "+"</td>");
-						$(that).append("<td>"+" "+ "</td>");
-						}
-					
-				});
-				});
+                            if (courseType.trim() !== 'Recitation' && courseType.trim() !== 'Laboratory') {
+                                var classes = data.result.values;
+                                var qualityavg = 0;
+                                var difficultyavg = 0;
+                                var numcourses = 0;
 
-          /* $('.pitDarkDataTable').tablesorter({
-           	    theme : 'blue',
-				format: function(s) { 
-            // format your data for normalization 
-            return s.toLowerCase().replace(/NA/,0); 
-        }           });
+                                profavg = 0;
+                                numprofcourses = 0;
+                                classes.forEach(function(name) {
+                                    var instructor = name.instructor.name.toLowerCase();
+                                    instructor = instructor.replace(/\./g, '');
 
-           $('.tablesorter-headerRow').css("color", "black");
+                                    if (instructor.trim() == inst.trim()) {
+                                        profavg += parseFloat(name.ratings.rInstructorQuality);
+                                        numprofcourses++;
+                                    }
 
-*/
-		}
-		}
-	}
-}, 10);
+
+
+                                    var alias = name.section.primary_alias.trim().slice(0, -4);
+                                    alias = alias.replace(/\s/g, '');
+                                    if (courseId == alias) {
+                                        qualityavg += parseFloat(name.ratings.rCourseQuality);
+                                        difficultyavg += parseFloat(name.ratings.rDifficulty);
+                                        numcourses++;
+                                    }
+                                });
+                                qualityavg = (qualityavg / numcourses).toFixed(2);
+                                difficultyavg = (difficultyavg / numcourses).toFixed(2);
+                                profavg = (profavg / numprofcourses).toFixed(2);
+                                if (isNaN(qualityavg)) {
+                                    qualityavg = "0";
+                                }
+                                if (isNaN(difficultyavg)) {
+                                    difficultyavg = "0";
+                                }
+                                if (isNaN(profavg)) {
+                                    profavg = "0";
+                                }
+
+
+                                $(that).append("<td>" + difficultyavg + "</td>");
+                                $(that).append("<td>" + qualityavg + "</td>");
+                                $(that).append("<td>" + profavg + "</td>");
+                            } else {
+                                $(that).append("<td>" + 0 + "</td>");
+                                $(that).append("<td>" + 0 + "</td>");
+                                $(that).append("<td>" + 0 + "</td>");
+                            }
+
+                        });
+                    });
+
+
+
+                    $(".pitDarkDataTable").tablesorter();
+
+
+                }
+
+            }
+        }
+    }, 10);
 });
