7be78f3120fd8e1f6383dce61bc462ad5aa5e165
Jonathan Dubin <saveprobe@gmail.com>
2014-02-23 12:09:36 -0500

    yo

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -99,69 +99,74 @@ To implement
 
 
                         } else {
+
+
+
                             $item = $.getJSON(chrome.extension.getURL('src/config/ratings.json'),
-                            function(ratings) {
-                                $(that).append("<td>" + ratings[courseId]['difficulty'] + "</td>");
-                                $(that).append("<td>" + ratings[courseId]['quality'] + "</td>");
-                            });
+                                function(ratings) {
+                                    $(that).append("<td>" + ratings[courseId]['difficulty'] + "</td>");
+                                    $(that).append("<td>" + ratings[courseId]['quality'] + "</td>");
+                                    console.log('getting info');
 
-                        //     $.ajax({
-                        //         type: 'GET',
-                        //         url: 'http://api.penncoursereview.com/v1/depts/' + courseDept + '/reviews?token=qL_UuCVxRBUmjWbkCdI554grLjRMPY',
-                        //         dataType: 'json',
-                        //         async: false
-                        //     }).done(function(data) {
+                                });
 
-                        //         var classes = data.result.values;
-                        //         var qualityavg = 0;
-                        //         var difficultyavg = 0;
-                        //         var numcourses = 0;
+                            //     $.ajax({
+                            //         type: 'GET',
+                            //         url: 'http://api.penncoursereview.com/v1/depts/' + courseDept + '/reviews?token=qL_UuCVxRBUmjWbkCdI554grLjRMPY',
+                            //         dataType: 'json',
+                            //         async: false
+                            //     }).done(function(data) {
 
-                        //         profavg = 0;
-                        //         numprofcourses = 0;
-                        //         classes.forEach(function(name) {
-                        //             var instructor = name.instructor.name.toLowerCase();
-                        //             instructor = instructor.replace(/\./g, '');
+                            //         var classes = data.result.values;
+                            //         var qualityavg = 0;
+                            //         var difficultyavg = 0;
+                            //         var numcourses = 0;
 
-                        //             if (instructor.trim() == inst.trim()) {
-                        //                 profavg += parseFloat(name.ratings.rInstructorQuality);
-                        //                 numprofcourses++;
-                        //             }
+                            //         profavg = 0;
+                            //         numprofcourses = 0;
+                            //         classes.forEach(function(name) {
+                            //             var instructor = name.instructor.name.toLowerCase();
+                            //             instructor = instructor.replace(/\./g, '');
 
-
-
-                        //             var alias = name.section.primary_alias.trim().slice(0, -4);
-                        //             alias = alias.replace(/\s/g, '');
-                        //             if (courseId == alias) {
-                        //                 qualityavg += parseFloat(name.ratings.rCourseQuality);
-                        //                 difficultyavg += parseFloat(name.ratings.rDifficulty);
-                        //                 numcourses++;
-                        //             }
-                        //         });
-                        //         qualityavg = (qualityavg / numcourses).toFixed(2);
-                        //         difficultyavg = (difficultyavg / numcourses).toFixed(2);
-                        //         profavg = (profavg / numprofcourses).toFixed(2);
-                        //         if (isNaN(qualityavg)) {
-                        //             qualityavg = "N/A";
-                        //         }
-                        //         if (isNaN(difficultyavg)) {
-                        //             difficultyavg = "N/A";
-                        //         }
-                        //         if (isNaN(profavg)) {
-                        //             profavg = "N/A";
-                        //         }
+                            //             if (instructor.trim() == inst.trim()) {
+                            //                 profavg += parseFloat(name.ratings.rInstructorQuality);
+                            //                 numprofcourses++;
+                            //             }
 
 
-                                // $(that).append("<td>" + rate['difficulty'] + "</td>");
-                                // $(that).append("<td>" + rate['quality'] + "</td>");
-                                // $(that).append("<td>" + profavg + "</td>");
 
-                        //     });
+                            //             var alias = name.section.primary_alias.trim().slice(0, -4);
+                            //             alias = alias.replace(/\s/g, '');
+                            //             if (courseId == alias) {
+                            //                 qualityavg += parseFloat(name.ratings.rCourseQuality);
+                            //                 difficultyavg += parseFloat(name.ratings.rDifficulty);
+                            //                 numcourses++;
+                            //             }
+                            //         });
+                            //         qualityavg = (qualityavg / numcourses).toFixed(2);
+                            //         difficultyavg = (difficultyavg / numcourses).toFixed(2);
+                            //         profavg = (profavg / numprofcourses).toFixed(2);
+                            //         if (isNaN(qualityavg)) {
+                            //             qualityavg = "N/A";
+                            //         }
+                            //         if (isNaN(difficultyavg)) {
+                            //             difficultyavg = "N/A";
+                            //         }
+                            //         if (isNaN(profavg)) {
+                            //             profavg = "N/A";
+                            //         }
+
+
+                            // $(that).append("<td>" + rate['difficulty'] + "</td>");
+                            // $(that).append("<td>" + rate['quality'] + "</td>");
+                            // $(that).append("<td>" + profavg + "</td>");
+
+                            //     });
                         }
 
 
                     });
-
+                    console.log('tablesort');
 
 
                     $(".pit").tablesorter({
