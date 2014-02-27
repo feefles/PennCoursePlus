e57c968136796889fcee701152d4264f88b76d20
Jonathan Dubin <saveprobe@gmail.com>
2014-02-26 22:21:58 -0500

    comments

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,16 +1,5 @@
 chrome.extension.sendMessage({}, function(response) {
 
-    // var getRating = function(course) {
-    //     $.ajax({
-    //         type: 'GET',
-    //         url: 'http://api.penncoursereview.com/v1/coursehistories/'+course+'?token=public',
-    //         // data: {query: },
-    //         dataType: 'json'
-    //     })
-
-    // )};
-
-
     /*
 To implement
     1. Link to PCR site
@@ -25,36 +14,34 @@ To implement
 */
 
     var readyStateCheckInterval = setInterval(function() {
+        //checks if we are on Penn InTouch website
         if (window.location.href.indexOf("pennintouch.apps.upenn.edu") > -1) {
-
 
 
             if (document.readyState === "complete") {
                 clearInterval(readyStateCheckInterval);
 
 
+                var title = $(document).find("title").text(); //grabs title of current page
+                var secondWord = title.split(' ')[1]; //grabs second word of the title
 
-                // ----------------------------------------------------------
-                // This part of the script triggers when page is done loading
-
-                if ($(".pitDarkDataTable").length > 0) {
-
-
+                if (secondWord == 'Search') { //checks if user is on the course search page
+                    
+                    //formats table so it can be sorted using TableSorter plugin
                     $('.pitDarkDataTable tr:first').unwrap().wrap("<thead/>");
-
                     $('thead').children().children().children().children().unwrap().wrap("<span/>");
-
-
                     $('.pitDarkDataTable').children().unwrap().wrapAll("<table class='tablesorter pit'/>");
-
-
                     $('.pit tr:nth-child(2)').nextUntil(".pit tr:last").andSelf().wrapAll("<tbody/>");
 
 
+                    //adds columns to table
                     $(".pit thead tr").append('<th id="difficulty">Difficulty</th>');
                     $(".pit thead tr").append('<th id="quality">Quality</th>');
                     //$(".pit thead tr").append('<th id="professor">Professor</th>');
 
+
+
+                
                     //$('#professor').tooltipster({
                     //  content: "Average quality of professor; higher is better."
                     //});
@@ -68,24 +55,23 @@ To implement
                     //fetches all courses in the course table
                     var courseList = $('.pit tbody').children();
 
-
-                    $(courseList).each(function() {
+                    //iterates through all courses in table      
+                    $(courseList).each(function() { 
 
                         var that = this;
-                        var courseId = $(this).find('td:first').text();
-                        var courseType = $(this).find('td:nth-child(3)').text();
-                        var inst = $(this).find('td:nth-child(4)').text().toLowerCase();
+                        var courseId = $(this).find('td:first').text(); //grabs course name from table
+                        var courseType = $(this).find('td:nth-child(3)').text(); //grabs course type (e.g. lecture, recitation)
+                        
+                        //sanitizes course information from table for lookup in JSON file
+                        var inst = $(this).find('td:nth-child(4)').text().toLowerCase(); 
                         inst = inst.replace(/\./g, '');
-
-
-
                         var courseId = courseId.trim();
                         courseId = courseId.slice(0, -4).replace(/\s/g, '');
                         courseDept = courseId.slice(0, -4).toLowerCase();
 
 
                         if (courseType.trim() == 'Recitation' || courseType.trim() == 'Laboratory') {
-
+                            //no ratings for recitations and laboratories 
                             $(that).append("<td>" + '' + "</td>");
                             $(that).append("<td>" + '' + "</td>");
                             // $(that).append("<td>" + '' + "</td>");
@@ -94,7 +80,7 @@ To implement
                         } else {
 
 
-
+                            //gets rating data for each course
                             $item = $.getJSON(chrome.extension.getURL('src/config/ratings.json'),
                                 function(ratings) {
                                     $(that).append("<td>" + ratings[courseId].difficulty + "</td>");
@@ -107,10 +93,10 @@ To implement
                                 url: chrome.extension.getURL('src/config/ratings.json'),
                                 async: false
                             }).done(function(data) {
-                                if (data[courseId] !== undefined) {
+                                if (data[courseId] !== undefined) { //adds course ratings to table
                                     $(that).append("<td>" + data[courseId].difficulty + "</td>");
                                     $(that).append("<td>" + data[courseId].quality + "</td>");
-                                } else {
+                                } else { //courses with no available data will be given value of "n/a"
                                     $(that).append("<td>" + "N/A" + "</td>");
                                     $(that).append("<td>" + "N/A" + "</td>");
                                 }
@@ -127,27 +113,25 @@ To implement
                     $(".pit").tablesorter({
                         headers: {
                             7: {
-                                sorter: "false",
+                                sorter: "false", //prevents sorting of "add to cart" column
                             },
                             8: {
-                                sorter: "digit",
-                                string: "bottom"
+                                sorter: "digit", //data in this column will be sorted numericall
+                                string: "bottom" //all non-numeric values will be pushed below sorted results
                             },
                             9: {
-                                sorter: "digit",
+                                sorter: "digit", 
                                 string: "bottom"
-                            }, // non - numeric content is treated as a MAX value
-                            //  10: {
-                            //    sorter: "digit",
-                            //  string: "bottom"
-                            // } // non-numeric content is treated as a MIN value
+                            }, 
                         },
                         // theme: "default"
                     });
+                    
                     $('.pit').show();
 
 
                 }
+                $('.pitDarkDataTable').show();
 
             }
         }
