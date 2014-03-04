38f43deaecd8458f7b494037e1a21c8598e17d6c
Jonathan Dubin <saveprobe@gmail.com>
2014-03-04 13:43:19 -0500

    Fixed bug

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -133,6 +133,15 @@ To implement
                 }
                 $('.pitDarkDataTable').show();
 
+                function css(selector, property, value) {
+                    for (var i=0; i<document.styleSheets.length;i++) {//Loop through all styles
+                        //Try add rule
+                        try { document.styleSheets[i].insertRule(selector+ ' {'+property+':'+value+'}', document.styleSheets[i].cssRules.length);
+                        } catch(err) {try { document.styleSheets[i].addRule(selector, property+':'+value);} catch(err) {}}//IE
+                    }
+                }
+                css('.pitDarkDataTable', 'display', 'inline !important');
+
             }
         }
     }, 10);
